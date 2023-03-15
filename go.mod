3ff6cece75a8c9a6bde41b306a016dc00ab8e992
Antonio Cheong <acheong@student.dalat.org>
2023-03-15 13:22:09 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,40 @@
+module github.com/acheong08/ChatGPT-Proxy-V4
+
+go 1.20
+
+require (
+	github.com/bogdanfinn/fhttp v0.5.18
+	github.com/bogdanfinn/tls-client v1.3.6
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
+	github.com/gin-gonic/gin v1.9.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/bogdanfinn/utls v1.5.14 // indirect
+	github.com/bytedance/sonic v1.8.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.15.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/tam7t/hpkp v0.0.0-20160821193359-2b70b4024ed5 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.9 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"io"
+	"os"
+	"time"
+
+	http "github.com/bogdanfinn/fhttp"
+	tls_client "github.com/bogdanfinn/tls-client"
+
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	jar     = tls_client.NewCookieJar()
+	options = []tls_client.HttpClientOption{
+		tls_client.WithTimeoutSeconds(360),
+		tls_client.WithClientProfile(tls_client.Chrome_110),
+		tls_client.WithNotFollowRedirects(),
+		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
+	}
+	client, _    = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+	access_token = os.Getenv("ACCESS_TOKEN")
+	puid         = os.Getenv("PUID")
+)
+
+func main() {
+	// Automatically refresh the puid cookie
+	if access_token == "" {
+		go func() {
+			url := "https://chat.openai.com/backend-api/models"
+			req, _ := http.NewRequest(http.MethodGet, url, nil)
+			req.Header.Set("Host", "chat.openai.com")
+			req.Header.Set("origin", "https://chat.openai.com/chat")
+			req.Header.Set("content-type", "application/json")
+			req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+			// Set authorization header
+			req.Header.Set("Authorization", "Bearer "+access_token)
+			// Initial puid cookie
+			req.AddCookie(
+				&http.Cookie{
+					Name:  "_puid",
+					Value: puid,
+				},
+			)
+			for {
+				resp, err := client.Do(req)
+				if err != nil {
+					break
+				}
+				defer resp.Body.Close()
+				println("Got response: " + resp.Status)
+				if resp.StatusCode != 200 {
+					println("Error: " + resp.Status)
+					// Print response body
+					body, _ := io.ReadAll(resp.Body)
+					println(string(body))
+					break
+				}
+				// Get cookies from response
+				cookies := resp.Cookies()
+				// Find _puid cookie
+				for _, cookie := range cookies {
+					if cookie.Name == "_puid" {
+						puid = cookie.Value
+						println("puid: " + puid)
+						break
+					}
+				}
+				// Sleep for 6 hour
+				time.Sleep(6 * time.Hour)
+			}
+			println("Error: Failed to refresh puid cookie")
+		}()
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	handler := gin.Default()
+	handler.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	handler.Any("/api/*path", proxy)
+
+	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handler)
+}
+
+func proxy(c *gin.Context) {
+
+	var url string
+	var err error
+	var request_method string
+	var request *http.Request
+	var response *http.Response
+
+	url = "https://chat.openai.com/backend-api" + c.Param("path")
+	request_method = c.Request.Method
+
+	request, err = http.NewRequest(request_method, url, c.Request.Body)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	request.Header.Set("Host", "chat.openai.com")
+	request.Header.Set("Origin", "https://chat.openai.com/chat")
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Connection", "keep-alive")
+	request.Header.Set("Keep-Alive", "timeout=360")
+	request.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+	request.Header.Set("Authorization", c.Request.Header.Get("Authorization"))
+	if c.Request.Header.Get("Puid") != "" {
+		request.AddCookie(
+			&http.Cookie{
+				Name:  "_puid",
+				Value: puid,
+			},
+		)
+	} else {
+		request.AddCookie(
+			&http.Cookie{
+				Name:  "_puid",
+				Value: c.Request.Header.Get("Puid"),
+			},
+		)
+	}
+
+	response, err = client.Do(request)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	defer response.Body.Close()
+	c.Header("Content-Type", response.Header.Get("Content-Type"))
+	// Get status code
+	c.Status(response.StatusCode)
+	c.Stream(func(w io.Writer) bool {
+		// Write data to client
+		io.Copy(w, response.Body)
+		return false
+	})
+
+}
