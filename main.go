006d2426768787231eb45316ef84912b1445e528
Antonio Cheong <acheong@student.dalat.org>
2023-03-15 21:44:14 +0800

    bugfix

--- main.go
+++ main.go
@@ -116,7 +116,7 @@ func proxy(c *gin.Context) {
 	request.Header.Set("Keep-Alive", "timeout=360")
 	request.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 	request.Header.Set("Authorization", c.Request.Header.Get("Authorization"))
-	if c.Request.Header.Get("Puid") != "" {
+	if c.Request.Header.Get("Puid") == "" {
 		request.AddCookie(
 			&http.Cookie{
 				Name:  "_puid",
