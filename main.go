f812185e52a7a0b618415561b4433a1a85f45b82
– sanz <sanzbernward@gmail.com>
2023-04-06 08:44:08 +0800

    Improved puid handling and code cleanup
    Added puid refresh functionality, error handling, and streamlined code for better readability.

--- main.go
+++ main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 	"time"
 
-	http "github.com/bogdanfinn/fhttp"
 	tls_client "github.com/bogdanfinn/tls-client"
-
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	jar     = tls_client.NewCookieJar()
-	options = []tls_client.HttpClientOption{
+	jar          = tls_client.NewCookieJar()
+	options      = []tls_client.HttpClientOption{
 		tls_client.WithTimeoutSeconds(360),
 		tls_client.WithClientProfile(tls_client.Chrome_110),
 		tls_client.WithNotFollowRedirects(),
-		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
+		tls_client.WithCookieJar(jar),
 	}
 	client, _    = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
 	access_token = os.Getenv("ACCESS_TOKEN")
 	puid         = os.Getenv("PUID")
-	http_proxy        = os.Getenv("http_proxy")
+	http_proxy   = os.Getenv("http_proxy")
 )
 
 func main() {
@@ -36,58 +35,10 @@ func main() {
 		client.SetProxy(http_proxy)
 		println("Proxy set:" + http_proxy)
 	}
+
 	// Automatically refresh the puid cookie
 	if access_token != "" {
-		go func() {
-			url := "https://chat.openai.com/backend-api/models"
-			req, _ := http.NewRequest(http.MethodGet, url, nil)
-			req.Header.Set("Host", "chat.openai.com")
-			req.Header.Set("origin", "https://chat.openai.com/chat")
-			req.Header.Set("referer", "https://chat.openai.com/chat")
-			req.Header.Set("sec-ch-ua", `Chromium";v="110", "Not A(Brand";v="24", "Brave";v="110`)
-			req.Header.Set("sec-ch-ua-platform", "Linux")
-			req.Header.Set("content-type", "application/json")
-			req.Header.Set("content-type", "application/json")
-			req.Header.Set("accept", "text/event-stream")
-			req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
-			// Set authorization header
-			req.Header.Set("Authorization", "Bearer "+access_token)
-			// Initial puid cookie
-			req.AddCookie(
-				&http.Cookie{
-					Name:  "_puid",
-					Value: puid,
-				},
-			)
-			for {
-				resp, err := client.Do(req)
-				if err != nil {
-					break
-				}
-				defer resp.Body.Close()
-				println("Got response: " + resp.Status)
-				if resp.StatusCode != 200 {
-					println("Error: " + resp.Status)
-					// Print response body
-					body, _ := io.ReadAll(resp.Body)
-					println(string(body))
-					break
-				}
-				// Get cookies from response
-				cookies := resp.Cookies()
-				// Find _puid cookie
-				for _, cookie := range cookies {
-					if cookie.Name == "_puid" {
-						puid = cookie.Value
-						println("puid: " + puid)
-						break
-					}
-				}
-				// Sleep for 6 hour
-				time.Sleep(6 * time.Hour)
-			}
-			println("Error: Failed to refresh puid cookie")
-		}()
+		go refreshPuidCookie()
 	}
 
 	PORT := os.Getenv("PORT")
@@ -104,58 +55,105 @@ func main() {
 	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handler)
 }
 
+func refreshPuidCookie() {
+	url := "https://chat.openai.com/backend-api/models"
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		println("Error creating request:", err.Error())
+		return
+	}
+
+	setRequestHeaders(req)
+
+	for {
+		resp, err := client.Do(req)
+		if err != nil {
+			println("Error:", err.Error())
+			break
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			println("Error: " + resp.Status)
+			body, _ := io.ReadAll(resp.Body)
+			println(string(body))
+			break
+		}
+
+		// Get cookies from response
+		cookies := resp.Cookies()
+		// Find _puid cookie
+		for _, cookie := range cookies {
+			if cookie.Name == "_puid" {
+				puid = cookie.Value
+				println("puid: " + puid)
+				break
+			}
+		}
+		// Sleep for 6 hours
+		time.Sleep(6 * time.Hour)
+	}
+	println("Error: Failed to refresh puid cookie")
+}
+
 func proxy(c *gin.Context) {
+	url := "https://chat.openai.com/backend-api" + c.Param("path")
+	request_method := c.Request.Method
 
-	var url string
-	var err error
-	var request_method string
-	var request *http.Request
-	var response *http.Response
-
-	url = "https://chat.openai.com/backend-api" + c.Param("path")
-	request_method = c.Request.Method
-
-	request, err = http.NewRequest(request_method, url, c.Request.Body)
+	request, err := http.NewRequest(request_method, url, c.Request.Body)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-	request.Header.Set("Host", "chat.openai.com")
-	request.Header.Set("Origin", "https://chat.openai.com/chat")
-	request.Header.Set("Connection", "keep-alive")
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Keep-Alive", "timeout=360")
-	request.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+
+	setRequestHeaders(request)
 	request.Header.Set("Authorization", c.Request.Header.Get("Authorization"))
-	if c.Request.Header.Get("Puid") == "" {
-		request.AddCookie(
-			&http.Cookie{
-				Name:  "_puid",
-				Value: puid,
-			},
-		)
-	} else {
-		request.AddCookie(
-			&http.Cookie{
-				Name:  "_puid",
-				Value: c.Request.Header.Get("Puid"),
-			},
-		)
+
+	puidValue := c.Request.Header.Get("Puid")
+	if puidValue == "" {
+		puidValue = puid
 	}
 
-	response, err = client.Do(request)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-	defer response.Body.Close()
-	c.Header("Content-Type", response.Header.Get("Content-Type"))
-	// Get status code
-	c.Status(response.StatusCode)
-	c.Stream(func(w io.Writer) bool {
-		// Write data to client
-		io.Copy(w, response.Body)
-		return false
+	request.AddCookie(&http.Cookie{
+		Name:  "_puid",
+		Value: puidValue,
 	})
 
+	response, err := client.Do(request)
+	if err != nil {
+        c.JSON(500, gin.H{"error": err.Error()})
+        return
+    }
+    defer response.Body.Close()
+
+    // Check if the puid cookie is invalid
+    if response.StatusCode == 401 {
+        refreshPuidCookie()
+        response, err = client.Do(request)
+        if err != nil {
+            c.JSON(500, gin.H{"error": err.Error()})
+            return
+        }
+        defer response.Body.Close()
+    }
+
+    c.Header("Content-Type", response.Header.Get("Content-Type"))
+    c.Status(response.StatusCode)
+    c.Stream(func(w io.Writer) bool {
+        io.Copy(w, response.Body)
+        return false
+    })
+}
+
+func setRequestHeaders(req *http.Request) {
+    req.Header.Set("Host", "chat.openai.com")
+    req.Header.Set("origin", "https://chat.openai.com/chat")
+    req.Header.Set("referer", "https://chat.openai.com/chat")
+    req.Header.Set("sec-ch-ua", Chromium";v="110", "Not A(Brand";v="24", "Brave";v="110)
+    req.Header.Set("sec-ch-ua-platform", "Linux")
+    req.Header.Set("content-type", "application/json")
+    req.Header.Set("accept", "text/event-stream")
+    req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+    // Set authorization header
+    req.Header.Set("Authorization", "Bearer "+access_token)
 }
