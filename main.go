d230daa234af61ac45f4ec112ee2ea08713201ff
zhangzp <zhangzp@qq.com>
2023-04-11 15:13:27 +0800

    Fix issue that proxy may use expired authorization from request.

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func proxy(c *gin.Context) {
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("Keep-Alive", "timeout=360")
 	request.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-	request.Header.Set("Authorization", c.Request.Header.Get("Authorization"))
+	request.Header.Set("Authorization", "Bearer "+access_token)
 	if c.Request.Header.Get("Puid") == "" {
 		request.AddCookie(
 			&http.Cookie{
