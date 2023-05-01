9446b7eec5fa0551f8aa1c2422d31a97dd9457e5
Antonio <acheong@student.dalat.org>
2023-05-01 16:18:14 +0800

    user agent

--- main.go
+++ main.go
@@ -19,19 +19,12 @@ var (
 		tls_client.WithNotFollowRedirects(),
 		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
 	}
-	client, _    = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
-	admin_pass   = os.Getenv("ADMIN_PASS")
-	cf_clearance = os.Getenv("CF_CLEARANCE")
-	user_agent   = os.Getenv("USER_AGENT")
-	http_proxy   = os.Getenv("http_proxy")
+	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+	http_proxy = os.Getenv("http_proxy")
 )
 
 func main() {
-	if cf_clearance == "" || user_agent == "" {
-		println("CF_CLEARANCE and USER_AGENT environment variables are required")
-		os.Exit(1)
-	}
-	println(cf_clearance)
 
 	if http_proxy != "" {
 		client.SetProxy(http_proxy)
@@ -40,7 +33,7 @@ func main() {
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
-		PORT = "8080"
+		PORT = "9090"
 	}
 	handler := gin.Default()
 	handler.GET("/ping", func(c *gin.Context) {
@@ -49,27 +42,6 @@ func main() {
 
 	handler.Any("/api/*path", proxy)
 
-	handler.POST("/admin/update", func(c *gin.Context) {
-		if c.Request.Header.Get("Authorization") != admin_pass {
-			c.JSON(401, gin.H{"message": "unauthorized"})
-			return
-		}
-		type Update struct {
-			Value string `json:"value"`
-			Field string `json:"field"`
-		}
-		var update Update
-		c.BindJSON(&update)
-		if update.Field == "cf_clearance" {
-			cf_clearance = update.Value
-			// export environment variable
-			os.Setenv("CF_CLEARANCE", cf_clearance)
-		} else {
-			c.JSON(400, gin.H{"message": "invalid field"})
-			return
-		}
-		c.JSON(200, gin.H{"message": "updated"})
-	})
 	gin.SetMode(gin.ReleaseMode)
 	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handler)
 }
@@ -108,13 +80,6 @@ func proxy(c *gin.Context) {
 	request.Header.Set("sec-fetch-site", "same-origin")
 	request.Header.Set("sec-gpc", "1")
 	request.Header.Set("user-agent", user_agent)
-
-	request.AddCookie(
-		&http.Cookie{
-			Name:  "cf_clearance",
-			Value: cf_clearance,
-		},
-	)
 
 	response, err = client.Do(request)
 	if err != nil {
