be71aa6cae2f5f336a58aa94b7774ad671067b47
Antonio <acheong@student.dalat.org>
2023-06-12 18:22:41 +0800

    allow user set puid

--- main.go
+++ main.go
@@ -166,6 +166,10 @@ func proxy(c *gin.Context) {
 	if os.Getenv("PUID") != "" {
 		request.Header.Set("cookie", "_puid="+os.Getenv("PUID")+";")
 	}
+	puid_cookie, err := c.Request.Cookie("_puid")
+	if err == nil {
+		request.Header.Set("cookie", "_puid="+puid_cookie.Value+";")
+	}
 
 	response, err = client.Do(request)
 	if err != nil {
