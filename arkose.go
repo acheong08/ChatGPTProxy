2fc34584ec0e0bb6a4b5e0b0c651153b2e4aaa0c
Antonio <acheong@student.dalat.org>
2023-06-25 10:20:06 +0800

    update bda

--- arkose.go
+++ arkose.go
@@ -38,7 +38,7 @@ func get_arkose_token() (string, error) {
 	// arkose_data_base64 := base64_encode(arkose_data)
 	// println(arkose_data)
 	url := "https://tcr9i.chat.openai.com/fc/gt2/public_key/35536E1E-65B4-4D96-9D97-6ADB7EFF8147"
-	payload := "bda=" + bda + "public_key=35536E1E-65B4-4D96-9D97-6ADB7EFF8147&site=https%3A%2F%2Fchat.openai.com&userbrowser=Mozilla%2F5.0%20(X11%3B%20Linux%20x86_64%3B%20rv%3A114.0)%20Gecko%2F20100101%20Firefox%2F114.0&capi_version=1.5.2&capi_mode=lightbox&style_theme=default&rnd=" + random_float()
+	payload := "bda=" + bda + "&public_key=35536E1E-65B4-4D96-9D97-6ADB7EFF8147&site=https%3A%2F%2Fchat.openai.com&userbrowser=Mozilla%2F5.0%20(X11%3B%20Linux%20x86_64%3B%20rv%3A114.0)%20Gecko%2F20100101%20Firefox%2F114.0&capi_version=1.5.2&capi_mode=lightbox&style_theme=default&rnd=0.2304346100108898"
 	req, _ := http.NewRequest(http.MethodPost, url, strings.NewReader(payload))
 	req.Header.Set("Host", "tcr9i.chat.openai.com")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:114.0) Gecko/20100101 Firefox/114.0")
