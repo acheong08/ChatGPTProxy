05eb1d4b38040f16c89ba7e8091e8256a0c56996
Antonio <acheong@student.dalat.org>
2023-06-25 22:32:19 +0800

    random rnd

--- arkose.go
+++ arkose.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"math/rand"
 	"strings"
 
@@ -11,6 +10,15 @@ import (
 
 type arkose_response struct {
 	Token string `json:"token"`
+}
+
+func random_num(length int) string {
+	chars := "0123456789"
+	var result string
+	for i := 0; i < length; i++ {
+		result += string(chars[rand.Intn(len(chars))])
+	}
+	return result
 }
 
 // func random_int(min, max int) int {
@@ -29,16 +37,16 @@ type arkose_response struct {
 // 	return base64.StdEncoding.EncodeToString([]byte(data))
 // }
 
-func random_float() string {
-	return fmt.Sprintf("%f", rand.Float64())
-}
+// func random_float() string {
+// 	return fmt.Sprintf("%f", rand.Float64())
+// }
 
 func get_arkose_token() (string, error) {
 	// arkose_data := `{"ct":"","iv":"","s":""}`
 	// arkose_data_base64 := base64_encode(arkose_data)
 	// println(arkose_data)
 	url := "https://tcr9i.chat.openai.com/fc/gt2/public_key/35536E1E-65B4-4D96-9D97-6ADB7EFF8147"
-	payload := "bda=" + bda + "&public_key=35536E1E-65B4-4D96-9D97-6ADB7EFF8147&site=https%3A%2F%2Fchat.openai.com&userbrowser=Mozilla%2F5.0%20(X11%3B%20Linux%20x86_64%3B%20rv%3A114.0)%20Gecko%2F20100101%20Firefox%2F114.0&capi_version=1.5.2&capi_mode=lightbox&style_theme=default&rnd=0.2304346100108898"
+	payload := "bda=" + bda + "&public_key=35536E1E-65B4-4D96-9D97-6ADB7EFF8147&site=https%3A%2F%2Fchat.openai.com&userbrowser=Mozilla%2F5.0%20(X11%3B%20Linux%20x86_64%3B%20rv%3A114.0)%20Gecko%2F20100101%20Firefox%2F114.0&capi_version=1.5.2&capi_mode=lightbox&style_theme=default&rnd=0." + random_num(16)
 	req, _ := http.NewRequest(http.MethodPost, url, strings.NewReader(payload))
 	req.Header.Set("Host", "tcr9i.chat.openai.com")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:114.0) Gecko/20100101 Firefox/114.0")
