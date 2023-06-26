76b5bc55b391662f045742cca8db918558e2c00d
Antonio <acheong@student.dalat.org>
2023-06-26 18:48:16 +0800

    saved by @linweiyuan

--- arkose.go
+++ arkose.go
@@ -1,50 +1,38 @@
 package main
 
-import (
-	"bytes"
-	"encoding/json"
-	"io"
+import "encoding/json"
 
-	http "github.com/bogdanfinn/fhttp"
-)
+// import (
+// 	arkose "github.com/flyingpot/funcaptcha"
+// )
 
-type arkose_response struct {
-	Token string `json:"token"`
-}
+// func get_arkose_token() (string, error) {
+// 	options := arkose.GetTokenOptions{
+// 		PKey: "35536E1E-65B4-4D96-9D97-6ADB7EFF8147",
+// 		SURL: "https://tcr9i.chat.openai.com",
+// 		Headers: map[string]string{
+// 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+// 		},
+// 		Site: "https://chat.openai.com",
+// 	}
+// 	result, err := arkose.GetToken(&options)
+// 	println(result.Token)
+// 	return result.Token, err
+// }
 
 func get_arkose_token() (string, error) {
-	resp, err := client.Get("https://ai.fakeopen.com/api/arkose/params")
+	type arkose struct {
+		Token string `json:"token"`
+	}
+	var result arkose
+	resp, err := client.Get("https://arkose-token.linweiyuan.com/backup")
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
-	payload, _ := io.ReadAll(resp.Body)
-	url := "https://tcr9i.chat.openai.com/fc/gt2/public_key/35536E1E-65B4-4D96-9D97-6ADB7EFF8147"
-	req, _ := http.NewRequest(http.MethodPost, url, bytes.NewReader(payload))
-	req.Header.Set("Host", "tcr9i.chat.openai.com")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:114.0) Gecko/20100101 Firefox/114.0")
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Origin", "https://tcr9i.chat.openai.com")
-	req.Header.Set("DNT", "1")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Referer", "https://tcr9i.chat.openai.com/v2/1.5.2/enforcement.64b3a4e29686f93d52816249ecbf9857.html")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("TE", "trailers")
-	resp, err = client.Do(req)
+	err = json.NewDecoder(resp.Body).Decode(&result)
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
-	var arkose arkose_response
-	err = json.NewDecoder(resp.Body).Decode(&arkose)
-	if err != nil {
-		return "", err
-	}
-	println(arkose.Token)
-	return arkose.Token, nil
+	return result.Token, nil
 }
